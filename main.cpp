4c1dbe9c2f02978f89ae4d1b329cfc6b2a3d479b
Pavel <pasha.mysyagin@gmail.com>
2026-01-27 20:23:12 +0300

    Добавил мин, макс элементы

--- main.cpp
+++ main.cpp
@@ -5,25 +5,38 @@
 template <typename T>
 struct GroupInfo {
     int key;
-    int count = 0;
+    int count;
+    T minElem;
+    T maxElem;
 };
 
 template <typename T>
 void groupStats(const std::vector<T>& arr) {
     std::map<int, GroupInfo<T>> groups;
 
-    for (T x : arr) {
+    for (const T& x : arr) {
         int key = x % 10;
-        if (groups.find(key) != groups.end()) {
-            groups[key].count++;
+
+        auto it = groups.find(key);
+        if (it == groups.end()) {
+            groups[key] = GroupInfo<T>{key, 1, x, x};
         } else {
-            groups[key] = GroupInfo<T>{key, 1};
+            GroupInfo<T>& g = it->second;
+            g.count++;
+
+            if (x < g.minElem) g.minElem = x;
+            if (g.maxElem < x) g.maxElem = x;
         }
     }
 
-    for (const auto& group : groups) {
-        std::cout << "Key: " << group.second.key << ", "
-                  << "Count: " << group.second.count << std::endl;
+    for (const auto& pair : groups) {
+        const auto& g = pair.second;
+
+        std::cout << "Key: " << g.key << "\n";
+        std::cout << "Count: " << g.count << "\n";
+        std::cout << "Min: " << g.minElem << "\n";
+        std::cout << "Max: " << g.maxElem << "\n";
+        std::cout << "-----\n";
     }
 }
 
